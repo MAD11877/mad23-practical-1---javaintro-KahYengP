9eed635662259bcf7d4dd3efcf32cd0d03df71bf
KahYengP <115883831+KahYengP@users.noreply.github.com>
2023-04-21 23:46:03 +0800

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -17,7 +17,17 @@ public class Question4
      * Hint: 2 loops are required. System.out.print(...) to print on single line.
      */
      
-    Scanner in = new Scanner(System.in);
+      Scanner in = new Scanner(System.in);
+    System.out.println("ln: ");
+    int line = in.nextInt();      
+    for (int i= line-1; i>=0 ; i--)  
+    {    
+      for (int j=0; j<=i; j++)  
+      {  
+        System.out.print("*" + " ");  
+      }  
+      System.out.println();  
+    }
     
   }
 }
