ab94ce6999ae45275b502b76167037d444eb8501
KahYengP <115883831+KahYengP@users.noreply.github.com>
2023-04-21 23:49:55 +0800

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -11,7 +11,7 @@ public class Question4
     {    
       for (int j=0; j<=i; j++)  
       {  
-        System.out.print("*" + " ");  
+        System.out.print("*");  
       }  
       System.out.println();  
     }
